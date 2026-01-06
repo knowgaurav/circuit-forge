9a216a349791516ab72caf0294ee75718984ecd5
Gaurav Singh <sgaurav497@gmail.com>
2026-01-06 12:12:36 +0530

    fix(logging): remove verbose Web Vitals logging from frontend

--- frontend/src/components/providers/AxiomProvider.tsx
+++ frontend/src/components/providers/AxiomProvider.tsx
@@ -1,12 +1,5 @@
 "use client";
 
-import { WebVitals } from "@/lib/axiom/client";
-
 export function AxiomProvider({ children }: { children: React.ReactNode }) {
-  return (
-    <>
-      <WebVitals />
-      {children}
-    </>
-  );
+  return <>{children}</>;
 }
